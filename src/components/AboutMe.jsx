18f20147d4db53f259ad3b972710bfb030c9a6bd
NaimoonJannat <naimoonjannat@gmail.com>
2024-07-04 00:30:24 +0600

    About me done

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -16,29 +16,39 @@ const AboutMe = () => {
                                 />
                                 <div className="flex flex-col">
                                 <h1 className="text-2xl text-gray-800 font-semibold">Get to Know Me</h1>
-                                    <h4 className="text-lg text-gray-800 font-semibold text-center md:text-left">
-                                        Naimoon Jannat Prapti 
-                                    </h4>
                                     <p className="text-gray-800">
-                                        Sed non nibh iaculis, posuere diam vitae, consectetur neque.
-                                        Integer velit ligula, semper sed nisl in, cursus commodo
-                                        elit. Pellentesque sit amet mi luctus ligula euismod
-                                        lobortis ultricies et nibh.
+                                    I am Naimoon Jannat Prapti, living in Bangladesh. Currently, I am pursuing a B.Sc in Computer Science and Engineering. I have a passion for web development and am dedicated to continuously learning and improving my skills in this field. In my journey, I have been exploring various web technologies and frameworks, and I enjoy building interactive and dynamic web applications. My goal is to become a proficient full-stack developer and contribute to impactful projects.
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    {/* Education  */}
                     <div className="w-full md:w-1/2">
-                    <h1 className="text-2xl font-semibold">My Education</h1>
-                    <h2>B.Sc in CSE (3rd Year, 3rd semester)</h2>
-                    <h2>Manarat International University</h2>
+                        <div className="p-6 sm:p-12 text-gray-100">
+                            <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+                                <img
+                                    src="education.png"
+                                    alt=""
+                                    className="self-center flex-shrink-0 w-24 h-24 border md:justify-self-start border-gray-700"
+                                />
+                                <div className="flex flex-col">
+                                <h1 className="text-2xl text-gray-800 font-semibold">My Education</h1>
+                                    <h4 className="text-lg text-gray-800 font-semibold text-left">
+                                    B.Sc (3rd Year, 3rd semester) 
+                                    </h4>
+                                    <h4 className="text-gray-800">
+                                    Computer Science and Engineering 
+                                    </h4>
+                                    <p className="text-gray-800">Manarat International University</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-                {/* <div className="w-full md:w-1/2">
-                <h1 className="text-2xl font-semibold">Get to Know Me</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, aut, officiis quae nihil ab perspiciatis quam autem non totam mollitia ipsa tempore, quidem ducimus cumque quisquam dignissimos sequi obcaecati! Asperiores autem quia laborum dolores veritatis fugiat neque quibusdam dolorum laudantium?</p>
-            </div> */}
+ 
+
                 
             </div>
         </div>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 const Title = ({title}) => {
     return (
         <div className="text-center my-6">
-            <h1 className="text-3xl text-[#F12F78] md:text-4xl font-bold">{title}</h1>
+            <h1 className="text-4xl text-[#F12F78] md:text-4xl font-bold">{title}</h1>
         </div>
     );
 };
